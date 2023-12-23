a83769deec5ea123133ae98b1315e3d2f1a367ed
Maroš Bratko <maros.bratko@second-foundation.eu>
2023-12-23 21:19:45 +0100

    save

---
+++ models/Wheatley/repo/train.sh
@@ -0,0 +1,1 @@
+python -m jssp.train --device cpu --n_j 100 --n_m 20 --total_timesteps 1000000 --n_validation_env 10 --n_steps_episode 1600  --batch_size 1600 --seed 1   --exp_name_appendix EXAMPLE2 
