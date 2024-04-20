c0328ee36598b4054fd5c9e9aa66873aa5e8a4e1
Maros112358 <b.maros.1999@gmail.com>
2024-04-20 21:29:21 +0200

    save djsp

---
+++ data/scripts/djsp/mannwhitneyu.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from scipy.stats import kruskal, mannwhitneyu
+import numpy as np
+
+def mean_std(x):
+    return f"{x.mean():.2f} ± {x.std():.2f}"
+
+def correct_label(label: str) -> str:
+    if label == 'end_to_end_drl_for_fjsp':
+        return 'End-to-end-DRL-for-FJSP'
+    if label == 'fjsp_drl':
+        return 'fjsp-drl'
+    if label == 'ieee_icce_rl_jsp':
+        return 'IEEE-ICCE-RL-JSP'
+        
+    return label.replace('_', '-')
+
+if __name__ == '__main__':  
+    # load data
+    df = pd.read_csv('djsp_preprocess_data.csv')
+
+
+    test_results = pd.DataFrame(columns=["Size", "Load factor", "First model", "Second model", "p-value"])
+    index = 0
+    for load_factor in [1, 2, 4]:
+        load_factor_group = df[df['load_factor'] == load_factor]
+        for size, size_group in load_factor_group.groupby(['jobs', 'machines']):
+            jobs, machines = size
+            
+            test_groups = []
+            models = []
+            for model, model_group in size_group.groupby(['model']):
+                test_groups.append(model_group['makespan'])
+                models.append(model)        
+
+            _, pvalue = kruskal(*test_groups)
+
+            
+            for i in range(len(models)):
+                for j in range(i + 1, len(models)):
+                    _, pvalue = mannwhitneyu(test_groups[i], test_groups[j], alternative='two-sided')
+                    if pvalue > 0.05:
+                        test_results.loc[index] = [
+                            f"{jobs}x{machines}",
+                            load_factor,
+                            correct_label(models[i][0]),
+                            correct_label(models[j][0]),
+                            f"{np.round(pvalue * 100)}$\%$"
+                        ]
+                        index += 1
+
+    print(test_results.to_latex(index=False))
